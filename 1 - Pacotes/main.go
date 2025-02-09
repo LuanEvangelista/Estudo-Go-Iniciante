0a6bbeab1b2d1a2ea5e61dc837184a12d6900d84
Luan Pereira Evangelista <luanpereiraevangelista@gmail.com>
2025-02-09 12:10:04 -0300

    Aula Concluida M2A5

--- 1 - Pacotes/main.go
+++ 1 - Pacotes/main.go
@@ -3,9 +3,15 @@ package main
 import (
 	"fmt"
 	"modulo/auxiliar"
+
+	"github.com/badoux/checkmail"
 )
 
 func main() {
 	fmt.Println("Escrevendo do arquivo main")
 	auxiliar.Escrever()
+
+	erro := checkmail.ValidateFormat("Luan@gmail.com")
+	fmt.Println(checkmail.ValidateFormat("Luan@gmail.com"))
+
 }
